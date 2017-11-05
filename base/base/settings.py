e39b67a9a73e92b8ee10256b2b920a3507f50010
Joshua Efiong <joshefiong@gmail.com>
2017-11-05 13:11:38 +0000

    Disable CSRF protection

--- base/base/settings.py
+++ base/base/settings.py
@@ -49,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'faults.middleware.DisableCsrfCheck',
 ]
 
 ROOT_URLCONF = 'base.urls'
