0ac5169f755ef0d0e4946dea34ba5ac19a263e0c
Joshua Efiong <joshefiong@gmail.com>
2017-11-07 16:32:32 +0000

    Add CORS support to API

--- auditor/base/deployed.py
+++ auditor/base/deployed.py
@@ -39,10 +39,11 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'faults',
-    'rest_framework.authtoken',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
